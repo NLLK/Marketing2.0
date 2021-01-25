a5fd2d36e13552f590f7e62a5edf535592be359d
Меркулов Дмитрий <dims.merkulov@gmail.com>
2021-01-25 16:28:17 +0600

    dorova

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,6 @@ namespace MegaMarketing2Reborn
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ Excel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace MegaMarketing2Reborn
+{
+    class Excel
+    {
+		private _Excel.Application app = null;
+		private _Excel.Workbook workbook = null;
+		private _Excel.Worksheet worksheet = null;
+		private _Excel.Range workSheet_range = null;
+		public void CreateExcelDoc()
+		{
+			createDoc();
+		}
+		public void createDoc()
+		{
+			try
+			{
+				app = new _Excel.Application();
+				app.Visible = true;
+				workbook = app.Workbooks.Add(1);
+				worksheet = (_Excel.Worksheet)workbook.Sheets[1];
+			}
+			catch (Exception e)
+			{
+				Console.Write("Error");
+			}
+			finally
+			{
+			}
+		}
+	}
+}
