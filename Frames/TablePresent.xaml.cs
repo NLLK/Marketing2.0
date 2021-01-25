4ae78d9f56ed598346f8e15a63e867b8e1b3e310
NLLK11 <babanov-artem@yandex.ru>
2021-01-25 15:51:42 +0600

    add tablepreesent page

---
+++ Frames/TablePresent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MegaMarketing2Reborn.Frames
+{
+    /// <summary>
+    /// Логика взаимодействия для Ta.xaml
+    /// </summary>
+    public partial class Ta : Page
+    {
+        public Ta()
+        {
+            InitializeComponent();
+        }
+    }
+}
