129c6a100a127b976147e70b9cb8039033f9bd3f
Barry Allen <babanov-artem@yandex.ru>
2021-07-03 21:04:06 +0600

    change sub project

--- WebApp/Properties/AssemblyInfo.cs
+++ WebApp/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется с помощью 
+// Управление общими сведениями о сборке осуществляется следующим образом
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных с этой сборкой.
-[assembly: AssemblyTitle("MegaMarketingWebApp")]
+[assembly: AssemblyTitle("WebApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MegaMarketingWebApp")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("WebApp")]
+[assembly: AssemblyCopyright("© , 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
-// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
 // COM, задайте атрибуту ComVisible значение true для требуемого типа.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
-[assembly: Guid("a010e0f6-a4e9-4bb9-a11b-fe0e629e8c78")]
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("d6bd8c9a-d286-46d4-a487-917f07a341e1")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
 //      основной номер версии;
-//      дополнительный номер версии;
+//      Дополнительный номер версии
 //      номер сборки;
 //      редакция.
 //
-// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
 // используя "*", как показано ниже:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
