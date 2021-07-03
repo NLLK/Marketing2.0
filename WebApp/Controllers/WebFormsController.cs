5388e1070445b509c1537937c38e106014a80d96
Barry Allen <babanov-artem@yandex.ru>
2021-07-03 21:04:06 +0600

    change sub project

---
+++ WebApp/Controllers/WebFormsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class WebFormsController : ApiController
+    {
+        // GET: api/WebForms
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/WebForms/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/WebForms
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/WebForms/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/WebForms/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
